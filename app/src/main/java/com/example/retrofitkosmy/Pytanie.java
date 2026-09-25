ae9789c1de877433525ddbd8edf55c4e97a33172
Kosmatheo <Kosmafzar@gmail.com>
2026-09-25 11:46:19 +0200

    Konstruktor

--- app/src/main/java/com/example/retrofitkosmy/Pytanie.java
+++ app/src/main/java/com/example/retrofitkosmy/Pytanie.java
@@ -1,4 +1,24 @@
 package com.example.retrofitkosmy;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Pytanie {
+    @SerializedName("tresc")
+    public String tresc;
+    @SerializedName("odp_a")
+    public String odpA;
+    @SerializedName("odp_b")
+    public String odpB;
+    @SerializedName("odp_c")
+    public String odpC;
+    public int odpowiedzPoprawna;
+
+    public Pytanie(String tresc, String odpA, String odpB, String odpC, int odpowiedzPoprawna) {
+        this.tresc = tresc;
+        this.odpA = odpA;
+        this.odpB = odpB;
+        this.odpC = odpC;
+        this.odpowiedzPoprawna = odpowiedzPoprawna;
+    }
+    //https://my-json-server.typicode.com/Kosmatheo/pytania_retrofit
 }
